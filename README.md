82e6c42558bea0c6ea7746ac8601c1293d839f1a
JonathanSchubert <jonathanschubert@web.de>
2016-08-21 14:12:31 +0200

    added docu, cleaned up libs

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # intersect-shapes-grid
 
 ### Description
+Demo code for:
+- intersecting shapes/polygons with grid data
+- visualizing the results
+in Python (without PostGIS or other GIS software).
+
+For intersecting only matplotlib standard functionality was used. Shapely lib was also tested, but turned out to be slower.
+Intersecting here just means counting the number of grid points within a polygon.
+
+In this example only dummy grid data and 3-digit post code shapes were used.
+Post code shapes with full resolution are also included in the repo.
+Instead of the used regular dummy grid data each other geo referenced point data can easily be implemented.
 
 ### Result
-![Alt text](result.png?raw=true "Title")
+![Alt text](result.png?raw=true "Result")
 
-### Source post code data
+### Source shape data
+http://www.suche-postleitzahl.org/downloads
--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 import numpy as np
 import pandas as pd
-import matplotlib.path as mplPath
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from mpl_toolkits.basemap import Basemap
 import shapefile
 import time
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
 import warnings
 
 ##################################
 # Define dummy grid data over Germany
 ##################################
-lat_start = 5
-lat_end = 15.1
-lon_start = 47.0
-lon_end = 55.1
+lat_start, lat_end = 5, 15.1
+lon_start, lon_end = 47.0, 55.1
 step = 0.05
-testpoint_grid = np.empty((0, 2), int)
+dummy_grid = np.empty((0, 2))
 for lat in np.arange(lat_start, lat_end, step):
     for lon in np.arange(lon_start, lon_end, step):
-        testpoint_grid = np.vstack((testpoint_grid, [lat, lon]))
-print('Created dummy grid:\t' + str(len(testpoint_grid)) + ' points')
+        dummy_grid = np.vstack((dummy_grid, [lat, lon]))
+print('Created dummy grid:\t %d points' % len(dummy_grid))
 
+##################################
 # Read shapefile of post code areas
+##################################
 file = "shape_data/plz-3stellig/plz-3stellig"
 # file = "shape_data/plz-2stellig/plz-2stellig"
 # file = "shape_data/plz-gebiete/plz-gebiete"
 sf = shapefile.Reader(file + '.shp')
 records = sf.records()
 shapes = sf.shapes()
-print('Imported post codes:\t' + str(len(records)) + ' shapes')
+print('Imported post codes:\t %d shapes' % len(records))
 
 ##################################
 # use matplotlib for counting grid points within post code area polygon
@@ -40,14 +37,14 @@ print('Start intersecting pc shapes with grid data...')
 start_time = time.time()
 pc_size = {}
 for i in range(0, len(records)):
-    path = mplPath.Path(shapes[i].points)
+    path = mpl.path.Path(shapes[i].points)
     pc = records[i][0]
     ext = path.get_extents()
-    subset_points = testpoint_grid[
-        (testpoint_grid[:, 0] >= (ext.extents[0] - 0.01)) &
-        (testpoint_grid[:, 0] <= (ext.extents[2] + 0.01)) &
-        (testpoint_grid[:, 1] >= (ext.extents[1] - 0.01)) &
-        (testpoint_grid[:, 1] <= (ext.extents[3] + 0.01))]
+    subset_points = dummy_grid[
+        (dummy_grid[:, 0] >= (ext.extents[0] - 0.01)) &
+        (dummy_grid[:, 0] <= (ext.extents[2] + 0.01)) &
+        (dummy_grid[:, 1] >= (ext.extents[1] - 0.01)) &
+        (dummy_grid[:, 1] <= (ext.extents[3] + 0.01))]
     points_within_pc = path.contains_points(subset_points)
     pc_size[pc] = len(subset_points[points_within_pc])
 
@@ -63,7 +60,7 @@ start_time = time.time()
 fig = plt.figure(figsize=(33, 18))
 bot, top, left, right = 5.85, 15.05, 47.25, 55.07
 ax = fig.add_subplot(121, axisbg='w', frame_on=False)
-ax.set_title('Dummy grid and post code shapes', fontsize=24, y=1.03)
+ax.set_title('Post code shapes and dummy grid', fontsize=24, y=1.03)
 m = Basemap(projection='merc', resolution='i',          # res: l, i, h
             llcrnrlat=left, llcrnrlon=bot,
             urcrnrlat=right, urcrnrlon=top)
@@ -73,7 +70,7 @@ m.fillcontinents(color='lightgray')
 m.readshapefile(file, 'units', color='black')
 
 # Add grid points to plot
-x, y = m(testpoint_grid[:, 0], testpoint_grid[:, 1])
+x, y = m(dummy_grid[:, 0], dummy_grid[:, 1])
 m.scatter(x, y, color='black', s=3, zorder=4)
 
 ##################################
@@ -100,8 +97,8 @@ for record, shape in zip(m.units_info, m.units):
     else:
         color = scheme[pc_size_df.ix[postcode]['bin']]
 
-    patches = [Polygon(np.array(shape), True)]
-    pc = PatchCollection(patches, zorder=2)
+    patches = [mpl.patches.Polygon(np.array(shape), True)]
+    pc = mpl.collections.PatchCollection(patches, zorder=2)
     pc.set_facecolor(color)
     pc.set_edgecolor(color)
     ax2.add_collection(pc)
